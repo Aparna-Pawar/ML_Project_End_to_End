341aeb8aad3d946a1731494750663d18061cd507
Aparna-Pawar <aparnapawar0408@gmail.com>
2026-02-13 19:47:37 +0530

    Adding Files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ML_Project_End_to_End',
+    version = '0.0.1',
+    author = 'Aparna',
+    author_email = 'aparnapawar0408@gmail.com',
+    packages = find_packages(),
+    install_requires = ['pandas','numpy','seaborn']
+)
